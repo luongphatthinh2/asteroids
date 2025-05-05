d9546be8045a9288219fb253ab754757283b6c4c
luongphatthinh2 <15520839@gm.uit.edu.vn>
2025-05-05 18:10:09 +0000

    G: add rotation left and right

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 import pygame
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -15,3 +15,16 @@ class Player(CircleShape):
         return [a, b, c]
     def draw(self, screen):
         pygame.draw.polygon(screen,"white", self.triangle(),2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            # ?
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            # ?
+            self.rotate(dt)
